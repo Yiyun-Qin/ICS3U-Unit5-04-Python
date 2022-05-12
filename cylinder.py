a4f52d97d50822c36826f7ed6d922dc6acdde18a
Yiyun Qin <yiyun.qin@mths.ca>
2022-05-12 18:22:26 +0000

    volume cylinder

--- cylinder.py
+++ cylinder.py
@@ -9,14 +9,19 @@ import math
 
 def VolumeCylinder(radius, height):
     # This function calculates the area of the triangle
+
+    # process
     volume = math.pi * pow(radius, 2) * height
+
+    # output
     return volume
 
 
 def main():
-    # This function does try and catch
+    # This function does try and catch, input and output
     print("This function calculates the volume of the cylinder.")
     print("Please enter the radius and the height.")
+    print("")
 
     # input
     radius_string = input("Enter the radius of the cylinder (cm): ")
@@ -28,9 +33,15 @@ def main():
         radius_integer = int(radius_string)
         height_integer = int(height_string)
         # call functions
-        volume = VolumeCylinder(radius = radius_integer, height = height_integer)
+        volume = VolumeCylinder(radius=radius_integer, height=height_integer)
+        print(
+            "The volume of this cylinder with the radius of {0} cm and the height of {1} cm is {2:,.2f} cm².".format(
+                radius_string, height_string, volume
+            )
+        )
     except Exception:
         print("Invalid number!")
+    finally:
         print("\nDone.")
 
 
